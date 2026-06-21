835e6426e39830efec4b56db7aa1f83f49635429
artiehumphreys <artieh2003@gmail.com>
2026-06-21 17:05:52 -0400

    feat(rasterizer): improved scene + refactoring

--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -1,7 +1,12 @@
 #include "rasterizer.hpp"
 
+static const Triangle scene[] = {
+    {{600, 200}, {200, 200}, {400, 400}, {255, 0, 0, 255}},
+    {{900, 300}, {1300, 300}, {1100, 700}, {0, 255, 0, 255}},
+    {{300, 600}, {700, 800}, {300, 900}, {0, 0, 255, 255}},
+};
+
 void rasterizer(FrameBuffer<1920, 1080> &fb) {
   fb.clear();
-  draw_triangle(fb,
-                Triangle{{600, 200}, {200, 200}, {400, 400}, {255, 0, 0, 255}});
+  draw_triangles(fb, scene, sizeof(scene) / sizeof(scene[0]));
 }
