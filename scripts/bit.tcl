4a83e03da1ae9584e558374b3e44260cb907c974
artiehumphreys <artieh2003@gmail.com>
2026-06-24 19:11:25 -0400

    feat(bit): bitstream config file

---
+++ scripts/bit.tcl
@@ -0,0 +1,13 @@
+open_project build_vivado/rasterizer_fpga.xpr
+
+launch_runs synth_1 -jobs 8
+wait_on_run synth_1
+
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
+
+if {[get_property PROGRESS [get_runs impl_1]] ne "100%"} {
+  error "bit.tcl: impl_1 did not finish (check timing/DRC in the run log)."
+}
+
+puts "bit.tcl: bitstream at build_vivado/rasterizer_fpga.runs/impl_1/system_wrapper.bit"
