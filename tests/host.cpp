874925c29342f9788f0fe91aa3564aa69d6c9197
artiehumphreys <artieh2003@gmail.com>
2026-07-09 20:12:52 -0400

    fix(tests): decouple scanout / rasterizer tests

--- tests/host.cpp
+++ tests/host.cpp
@@ -1,6 +1,5 @@
 #include "framebuffer.hpp"
 #include "rasterizer.hpp"
-#include "scanout.hpp"
 #include <cassert>
 
 void test_triangle_rasterization() {
@@ -81,36 +80,10 @@ void test_offscreen_triangle_draws_nothing() {
   }
 }
 
-void test_read_line() {
-  const int W = 5, H = 5;
-  Pixel storage[W * H];
-  FrameBuffer<W, H> fb{storage};
-
-  for (int y = 0; y < H; ++y) {
-    for (int x = 0; x < W; ++x) {
-      fb.data[fb.get_pixel_addr(x, y)] = x + y * W;
-    }
-  }
-
-  hls::stream<video_packet_t> video_out;
-  read_lines(fb, video_out);
-
-  for (int y = 0; y < H; ++y) {
-    for (int x = 0; x < W; ++x) {
-      auto packet = video_out.read();
-      assert(packet.data == x + y * W);
-      assert(packet.user == (y == 0 && x == 0));
-      assert(packet.last == (x == W - 1));
-    }
-  }
-  assert(video_out.empty());
-}
-
 int main() {
   test_triangle_rasterization();
   test_draw_triangles_draws_all();
   test_clips_offscreen_triangle();
   test_offscreen_triangle_draws_nothing();
-  test_read_line();
   return 0;
 }
