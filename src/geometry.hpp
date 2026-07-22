1da4ea56279c134b778b939fb4303368b03dfd9e
artiehumphreys <artieh2003@gmail.com>
2026-07-22 16:09:57 -0500

    feat(geometry): barycentric coordinate helpers, new typing

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -11,21 +11,50 @@ struct Triangle {
   Pixel color;
 };
 
+struct Bary {
+  float u, v, w;
+};
+
+struct SubAreas {
+  int bcp, cap, abp;
+};
+
 constexpr int calculate_cross_product(Point A, Point B, Point P) {
   int lhs = (B.x - A.x) * (P.y - A.y);
   int rhs = (B.y - A.y) * (P.x - A.x);
   return lhs - rhs;
 }
 
-constexpr bool is_inside_triangle(Triangle T, Point p) {
+constexpr void get_sub_areas(const Triangle &T, Point p, SubAreas &areas) {
+  int ABP = calculate_cross_product(T.a, T.b, p);
+  int BCP = calculate_cross_product(T.b, T.c, p);
+  int CAP = calculate_cross_product(T.c, T.a, p);
+  areas = {BCP, CAP, ABP};
+}
+
+constexpr void get_barycentric_coordinates(const Triangle &T, Point p,
+                                           Bary &coords) {
+  // https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle//barycentric-coordinates.html
+  SubAreas areas;
+  get_sub_areas(T, p, areas);
+
+  float ABC = static_cast<float>(areas.bcp) + areas.cap + areas.abp;
+  // u -> BCP / ABC, v -> CAP / ABC, w -> ABP / ABC
+  float u = areas.bcp / ABC;
+  float v = areas.cap / ABC;
+  float w = areas.abp / ABC;
+
+  coords = {u, v, w};
+}
+
+constexpr bool is_inside_triangle(const Triangle &T, Point p) {
   // NOTE: Instead of depending on CW ordering of points, just check that
   // determinant signs match
-  int d0 = calculate_cross_product(T.a, T.b, p);
-  int d1 = calculate_cross_product(T.b, T.c, p);
-  int d2 = calculate_cross_product(T.c, T.a, p);
+  SubAreas areas;
+  get_sub_areas(T, p, areas);
 
-  bool has_neg = (d0 < 0) || (d1 < 0) || (d2 < 0);
-  bool has_pos = (d0 > 0) || (d1 > 0) || (d2 > 0);
+  bool has_neg = (areas.bcp < 0) || (areas.cap < 0) || (areas.abp < 0);
+  bool has_pos = (areas.bcp > 0) || (areas.cap > 0) || (areas.abp > 0);
 
   return !(has_neg && has_pos); // mixed signs -> outside
 }
