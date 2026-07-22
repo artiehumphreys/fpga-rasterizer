b3ff51614afa46dbe95b0644efc2dc291c942e0e
artiehumphreys <artieh2003@gmail.com>
2026-07-21 21:41:38 -0500

    feat(rasterizer): dynamic scene for tearing test

--- src/scene.hpp
+++ src/scene.hpp
@@ -3,7 +3,7 @@
 #include "geometry.hpp"
 #include "pixel.hpp"
 
-inline constexpr Triangle scene[] = {
+inline Triangle scene[] = {
     {{400, 133}, {133, 133}, {267, 267}, rgba(255, 0, 0, 255)},
     {{600, 200}, {867, 200}, {733, 467}, rgba(0, 255, 0, 255)},
     {{200, 400}, {467, 533}, {200, 600}, rgba(0, 0, 255, 255)},
