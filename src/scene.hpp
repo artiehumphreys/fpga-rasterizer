49d3f294c0fa3461699d5dd3e8b7e37e8e7f0ae6
artiehumphreys <artieh2003@gmail.com>
2026-07-23 15:12:30 -0500

    fix(scene): fixed triangle declarations

--- src/scene.hpp
+++ src/scene.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
 #include "geometry.hpp"
+#include "pixel.hpp"
 
-inline Triangle scene[] = {
-    {{400, 133}, {133, 133}, {267, 267}, {0.5f, 0.0f, 0.75f}},
-    {{600, 200}, {867, 200}, {733, 467}, {1.0f, 0.25f, 0.5f}},
-    {{200, 400}, {467, 533}, {200, 600}, {0.4f, 0.6f, 0.8f}}};
+inline Triangle scene[] = {{{400, 133},
+                            {133, 133},
+                            {267, 267},
+                            {0.5f, 0.0f, 0.75f},
+                            rgba(0xFF, 0x00, 0x00, 0x00)},
+                           {{600, 200},
+                            {867, 200},
+                            {733, 467},
+                            {1.0f, 0.25f, 0.5f},
+                            rgba(0x00, 0xFF, 0x00, 0x00)},
+                           {{200, 400},
+                            {467, 533},
+                            {200, 600},
+                            {0.4f, 0.6f, 0.8f},
+                            rgba(0x00, 0x00, 0xFF, 0x00)}};
 inline constexpr int scene_count = sizeof(scene) / sizeof(scene[0]);
